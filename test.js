e95ec448e021c6a653362a70c53c78f178557a48
joanna-by <j.bykowska@gmail.com>
2018-10-02 09:24:51 +0200

    else if statement

--- test.js
+++ test.js
@@ -1,23 +1,19 @@
-var youLikeMeat = false;
-var myNum = 10;
+var myAge = 49;
 
-if (myNum == 9 ){
+if (myAge > 30){
 
-    document.write("myNum is equal to 10")
+    document.write("you are over 30");
 
-} else{
-    
-    document.write("myNum is not equal ti 10")
+} else if (myAge > 20){
 
-}
+    document.write("you are over 20");
 
-if (youLikeMeat){
+} else if (myAge > 10){
 
-    document.write("Here is a meaty menu...")
+    document.write("you are over 10");
 
 } else{
 
-    document.write("Here is a vege menu...")
+    document.write("you are not over 10");
 }
-
  
