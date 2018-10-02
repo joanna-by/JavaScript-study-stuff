124664cddb05203d9b81fecf12c14cb48c6c42a7
joanna-by <j.bykowska@gmail.com>
2018-10-02 20:16:06 +0200

    function

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
-function getAverage (a,b) {
+function getAverage (a,b,c,d,e,f) {
 
-    var average = (a + b) / 2;
+    var average = (a + b + c + d + e +f) / 6;
     console.log(average);
+    return average;
 
 }
 
-getAverage(7,8);
-alert("Hello, world!")
+var myResult = getAverage(7,8,13,23,24,5);
+console.log("the average is " + myResult);
