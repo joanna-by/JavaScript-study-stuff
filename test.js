2d065dcb5157ac11239928e31437fe2974f7d5f8
joanna-by <j.bykowska@gmail.com>
2018-10-02 09:06:06 +0200

    if

--- test.js
+++ test.js
@@ -1,1 +1,23 @@
-alert("Hello, world!");
+var youLikeMeat = false;
+var myNum = 10;
+
+if (myNum == 9 ){
+
+    document.write("myNum is equal to 10")
+
+} else{
+    
+    document.write("myNum is not equal ti 10")
+
+}
+
+if (youLikeMeat){
+
+    document.write("Here is a meaty menu...")
+
+} else{
+
+    document.write("Here is a vege menu...")
+}
+
+ 
