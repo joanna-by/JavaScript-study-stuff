a96c804489a30a87dd57f67a89544ebfeca4ffb1
joanna-by <j.bykowska@gmail.com>
2018-10-04 11:22:41 +0200

    this word

--- test.js
+++ test.js
@@ -1,19 +1,30 @@
-var myCar = new Object ();
-myCar.maxSpeed = 50;
-myCar.driver = "Shaun";
-myCar.drive = function(){
-    console.log("now driving");
-};
-
-myCar.drive();
+console.log(this);
 
 myCar2 = {
+
     maxSpeed: 70, 
     driver: "Asia", 
     drive: function(speed, time){
         console.log(speed * time);
+    },
+    test: function(){
+        console.log(this);
     }
 };
 
+var myCar3 = {
+
+    maxSpeed: 45,
+    driver: "Tom",
+    drive: function(speed, time){
+        console.log(speed * time);
+    },
+    test: function(){
+        console.log(this);
+    }
+};
+
+myCar2.test();
+myCar3.test();
 console.log(myCar2.maxSpeed);
 myCar2.drive(50, 3);
